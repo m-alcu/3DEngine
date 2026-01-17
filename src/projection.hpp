420a617fb5fbf02d27f6e56ca54b8f7a3b2cbd35
Martin Alcubierre <martin.alcubierre@gmail.com>
2026-01-17 14:45:44 +0100

    Change div per mult (cheaper)

--- src/projection.hpp
+++ src/projection.hpp
@@ -15,8 +15,8 @@ public:
 
        if (p.broken || init) {
            float oneOverW = 1.0f / p.ndc.w;
-           float sx = (p.ndc.x * oneOverW + 1.0f) * (width / 2.0f) + 0.5f; // Convert from NDC to screen coordinates
-           float sy = (p.ndc.y * oneOverW + 1.0f) * (height / 2.0f) + 0.5f; // Convert from NDC to screen coordinates
+           float sx = (p.ndc.x * oneOverW * 0.5f + 0.5f) * width + 0.5f; // Convert from NDC to screen coordinates
+           float sy = (p.ndc.y * oneOverW * 0.5f + 0.5f) * height + 0.5f; // Convert from NDC to screen coordinates
 
            // Keep subpixel precision: 16.16 fixed-point
            constexpr float FP = 65536.0f; // 1<<16
